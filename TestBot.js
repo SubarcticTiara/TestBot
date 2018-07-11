5df18cbdb26ad65b7affdfc28fee14e479b9e5ff
SubarcticTiara <41029036+SubarcticTiara@users.noreply.github.com>
2018-07-11 18:31:35 +0300

    Create TestBot.js

---
+++ TestBot.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+Client.on("ready", () => {
+    console.log("I'm ready!");
+});
+
+client.login(process.env.NDY0NDEwMzU1Njg3NDg5NTM4.Dh_tpA.ocBs-Ai30LNTqLWM-TuXyQ7Fcdo);
